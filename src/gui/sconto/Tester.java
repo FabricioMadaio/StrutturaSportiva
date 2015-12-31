b15e930d1b0b0b012513a4812670a5a13dbc4f94
Fabricio Nicolas Madaio <fabricio.madaio@gmail.com>
2015-12-31 12:05:02 +0100

    aggiornamenti daily scrum

---
+++ src/gui/sconto/Tester.java
@@ -0,0 +1,16 @@
+package gui.sconto;
+
+import javax.swing.JFrame;
+
+import core.ListaUtenti;
+import core.elementi.Posto;
+import core.elementi.Stadio;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+	
+	}
+
+}
