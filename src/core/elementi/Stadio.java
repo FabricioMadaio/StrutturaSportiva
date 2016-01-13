6f781948952f3f68d5c8e3e1806d1fa66f752cea
Giovanni Leo <johnny.1995@hotmail.it>
2016-01-13 18:21:49 +0100

    javadoc stadio e altre calassi

--- src/core/elementi/Stadio.java
+++ src/core/elementi/Stadio.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class Stadio implements Serializable
 {
-	
+
 	/**
 	 * 
 	 */
@@ -17,62 +17,103 @@ public class Stadio implements Serializable
 
 	private String pathImmagine;
 	private transient Image image;
-	
+
 	private ArrayList<Posto> posti;
-	
-	
+
+	/**
+	 * ######################## FABRI VEDI TU #####################
+	 * @param nome
+	 * @param prezzoBase
+	 * @param pathImmagine
+	 */
 	public Stadio(String nome, int prezzoBase,String pathImmagine) {
-		
+
 		this.posti = new ArrayList<Posto>();
 		this.nome = nome;
 		this.incasso = 0;
 		this.pathImmagine = pathImmagine;
 		this.setPrezzoBase(prezzoBase);
 	}
-	
+
+	/**
+	 * Il metodo ritorna il nome dello stadio.
+	 * @return String nome
+	 */
 	public String getNome() {
 		return nome;
 	}
 
+	/**
+	 * Il metodo ritorna la capienza dello stadio.
+	 * @return int capienza
+	 */
 	public int getCapienza()
 	{
 		capienza = posti.size();
 		return capienza;
 	}
-	
+
+	/**
+	 * Il metodo ritorna il prezzo base del biglietto dello stadio.
+	 * @return double prezzoBase
+	 */
 	public double getPrezzoBase() {
 		return prezzoBase;
 	}
-
+	/**
+	 * Il metodo permette di settare il prezzo base dello stadio.
+	 * @param double prezzoBase
+	 */
 	public void setPrezzoBase(double prezzoBase) {
 		this.prezzoBase = prezzoBase;
 	}
-	
+	/**
+	 * Il metodo ritorna la lista di posti associati allo stadio.
+	 * @return ArrayList<Posto> posti
+	 */
 	public ArrayList<Posto> getPosti() {
 		return posti;
 	}
-
+	/**
+	 * Il metto permette di settare la lista dei posti dello stadio.
+	 * @param ArrayList<Posto> posti
+	 */
 	public void setPosti(ArrayList<Posto> posti) {
 		this.posti = posti;
 	}
-	
+	/**
+	 * Il metodo permette di aggiungere un posto alla lista dei posto dello stadio.
+	 * @param Posto posto
+	 */
 	public void addPosto(Posto posto) {
 		this.posti.add(posto);
 	}
-
+	/**
+	 * Il metodo ritorna l'incasso dello stadio.
+	 * @return double incasso
+	 */
 	public double getIncasso() {
 		return incasso;
 	}
-
+	/**
+	 * Il metodo permette di settare l'incasso dello stadio.
+	 * @param double incasso
+	 */
 	public void setIncasso(double incasso) {
 		this.incasso = incasso;
 	}
-	
+	/**
+	 * Il metodo serve aricavare l'incasso totale.
+	 * @param double incasso
+	 */
 	public void aggiungiIncasso(double incasso) {
 		this.incasso += incasso;
 	}
-	
 
+	/**
+	 * ######################## Fabri i metodi restanti guardali tu ############
+	 * @return
+	 */
 	public String getPathImmagine() {
 		return pathImmagine;
 	}
@@ -88,5 +129,5 @@ public class Stadio implements Serializable
 	public void setImage(Image image) {
 		this.image = image;
 	}
-	
+
 }
--- src/core/sconti/GiornoDellaSettimaNonSpecificatoException.java
+++ src/core/sconti/GiornoDellaSettimaNonSpecificatoException.java
@@ -1,5 +1,8 @@
 package core.sconti;
-
+/**
+ * Questa eccezione viene chiamata quando un giorno 
+ * della settimana non è specificato correttamente.
+ */
 public class GiornoDellaSettimaNonSpecificatoException extends RuntimeException
 {
 	public GiornoDellaSettimaNonSpecificatoException() 
